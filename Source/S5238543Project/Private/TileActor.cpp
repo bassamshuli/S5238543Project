e1e6009f769e1ce0eae78758f686bed503741a15
Bassam <salimsh909@gmail.com>
2025-02-19 15:14:27 +0100

    GRIGLIA
    IN FASE

--- Source/S5238543Project/Private/TileActor.cpp
+++ Source/S5238543Project/Private/TileActor.cpp
@@ -9,6 +9,46 @@ ATileActor::ATileActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+	// template function that creates a components
+	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+
+	// every actor has a RootComponent that defines the transform in the World
+	SetRootComponent(Scene);
+	StaticMeshComponent->SetupAttachment(Scene);
+
+	Status = ETileStatus::EMPTY;
+	PlayerOwner = -1;
+	TileGridPosition = FVector2D(0, 0);
+
+
+}
+
+void ATileActor::SetTileStatus(const int32 TileOwner, const ETileStatus TileStatus)
+{
+	PlayerOwner = TileOwner;
+	Status = TileStatus;
+}
+
+ETileStatus ATileActor::GetTileStatus()
+{
+	return Status;
+}
+
+int32 ATileActor::GetOwner()
+{
+	return PlayerOwner;
+}
+
+void ATileActor::SetGridPosition(const double InX, const double InY)
+{
+	TileGridPosition.Set(InX, InY);
+
+}
+
+FVector2D ATileActor::GetGridPosition()
+{
+	return TileGridPosition;
 }
 
 // Called when the game starts or when spawned
--- Source/S5238543Project/Public/TileActor.h
+++ Source/S5238543Project/Public/TileActor.h
@@ -9,8 +9,8 @@
 UENUM()
 enum class ETileStatus : uint8
 {
-	empty UMETA(DisplayName = "empty"),
-	occupied  UMETA(DisplayName = "occupied"),
+	EMPTY UMETA(DisplayName = "EMPTY"),
+	OCCUPIED  UMETA(DisplayName = "OCCUPIED"),
 };
 UCLASS()
 class S5238543PROJECT_API ATileActor : public AActor
@@ -21,10 +21,42 @@ public:
 	// Sets default values for this actor's properties
 	ATileActor();
 
+	// set the player owner and the status of a tile
+	void SetTileStatus(const int32 TileOwner, const ETileStatus TileStatus);
+
+	// get the tile status
+	ETileStatus GetTileStatus();
+
+	// get the tile owner
+	int32 GetOwner();
+
+	// set the (x, y) position
+	void SetGridPosition(const double InX, const double InY);
+
+	// get the (x, y) position
+	FVector2D GetGridPosition();
+
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USceneComponent* Scene;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	ETileStatus Status;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 PlayerOwner;
+
+	// (x, y) position of the tile
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FVector2D TileGridPosition;
+
 
 //public:	
 	// Called every frame
